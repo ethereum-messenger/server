99a55eb3b70dcef1234859928f9311802e73cda6
Benjamin Reed <benvreed@gmail.com>
2018-01-27 17:37:08 -0600

    Add EthereumService skeleton

---
+++ services/ethereum_service.js
@@ -0,0 +1,21 @@
+class EthereumService {
+  constructor() {
+    // logic to setup, may want to make singleton, don't know
+  }
+
+  createRoom(userAddress) {
+
+  }
+
+  inviteUserToRoom(userAddress, roomAddress) {
+
+  }
+
+  readMessages(userAddress, roomAddress, optionalLastMessage) {
+
+  }
+
+  writeMessage(userAddress, roomAddress, message) {
+    
+  }
+}
