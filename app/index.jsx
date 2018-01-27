4f0f61b7f0cf55ab2da32be3bb284c10e1f1baca
Benjamin Reed <benvreed@gmail.com>
2018-01-27 14:49:47 -0600

    Add babel fix

--- app/index.jsx
+++ app/index.jsx
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import "../styles/index.scss";
+
+class Index extends Component {
+  render() {
+    return (
+      <h1>Hello, react</h1>
+    )
+  }
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
