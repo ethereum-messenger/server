27687386049c6f3d201449c2c0da3f60175a66f0
Benjamin Reed <benvreed@gmail.com>
2018-01-28 05:41:18 -0600

    Add CORS

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var api = require('./routes/api');
 
 var app = express();
+
+app.use(cors());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
