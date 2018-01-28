0f20d8d991ed35a0aff204afe913130f4449797b
Robert Osonma <robertosonma@main-10-231-54-120.tamulink.tamu.edu>
2018-01-27 23:22:15 -0600

    bug fixes

--- app/index.jsx
+++ app/index.jsx
@@ -1,27 +1,7 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import Application from './components/Application.jsx';
-=======
-import Test from './test.jsx';
-import Usertxt from './initialtxt.jsx';
-import Chattxt from './chatroom.jsx'
->>>>>>> To rebase
 import "../styles/index.scss";
-import './actions'
+//import './actions'
 
-<<<<<<< HEAD
 ReactDOM.render(<Application/>, document.getElementById('root'));
-=======
-class Index extends Component {
-  render() {
-    return (
-      <div>
-      <Test />
-      <Usertxt />
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<Index/>, document.getElementById('root'));
->>>>>>> To rebase
