659abcd01ed3b46dc21d48b8f01517a51648482c
Benjamin Reed <benvreed@gmail.com>
2018-01-28 02:54:43 -0600

    Fixed API routes

--- routes/api.js
+++ routes/api.js
@@ -3,23 +3,23 @@ const router = express.Router();
 const EthereumService = require('../services/ethereum_service');
 
 router.post('/rooms', async (req, res, next) => {
-  const userAddress = req.body.userAddress;
+  const {userAddress, keystore, password} = req.body;
   const service = new EthereumService();
+  console.log(service);
   try {
-    const roomAddress = await service.createRoom(userAddr);
+    const roomAddress = await service.createRoom(userAddress, keystore, password);
     res.json({userAddress, roomAddress});
   } catch (err) {
+    console.log(err);
     return res.status(500).send();
   }
 });
 
 router.post('/invitations', async (req, res, next) => {
-  const body = req.body;
-  const roomAddress = body.roomAddress;
-  const inviteeAddress = body.inviteeAddress;
+  const {userAddress, roomAddress, keystore, password, inviteeAddress} = req.body;
   const service = new EthereumService();
   try {
-    const response = await service.inviteUserToRoom(inviteeAddress, roomAddress);
+    const response = await service.inviteUserToRoom(userAddress, roomAddress, keystore, password, inviteeAddress);
     res.json(response);
   } catch (err) {
     return res.status(500).send();
@@ -27,25 +27,26 @@ router.post('/invitations', async (req, res, next) => {
 });
 
 router.get('/rooms/messages', async (req, res, next) => {
-  const userAddress = req.params.userAddress;
-  const roomAddress = req.params.roomAddress;
-  const optionalLastMessage = req.params.optionalLastMessage;
+  const {userAddress, roomAddress, optionalLastMessage} = req.query;
+  console.log(userAddress, roomAddress, optionalLastMessage);
+
   const service = new EthereumService();
   try {
     const messages = await service.readMessages(userAddress, roomAddress, optionalLastMessage);
     res.json({messages});
   } catch (err) {
+    console.log(err);
     return res.status(500).send();
   }
 });
 
 router.post('/rooms/messages', async (req, res, next) => {
-  const userAddress = req.params.userAddress;
-  const roomAddress = req.params.roomAddress;
-  const message = req.body.message;
+  const {userAddress, roomAddress, keystore, password} = req.body;
+  const message = `${userAddress}: ${req.body.message}`;
+
   const service = new EthereumService();
   try {
-    const response = await service.writeMessage(userAddress, roomAddress, message);
+    const response = await service.writeMessage(userAddress, roomAddress, keystore, password, message);
     res.json(response);
   } catch (err) {
     return res.status(500).send();
